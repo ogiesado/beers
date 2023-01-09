1077df3d72b79d616bb3b9fa54f1afdabae5403c
Ogie Sado <sado.ogie@gmail.com>
2023-01-09 14:09:41 +0000

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Beer</h1>
+      <h1 className="text-3xl font-bold">Beers</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
